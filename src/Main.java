0774401ce5cf6a35f31553fa27c5722f9e3c1977
MixaMmv <mmv.0097@gmail.com>
2021-09-18 14:23:43 +0500

    Primitive types

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.math.BigDecimal;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        String hello = "Hello";
+        String world = "world";
+
+        int a = 10;
+
+        double d1 = 1.00001;
+        double d2 = 1.00000999999;
+
+        BigDecimal one = new BigDecimal(d1);
+        BigDecimal two = new BigDecimal(d2);
+
+        StringBuilder builder = new StringBuilder(hello + ' ' + world);
+        builder.replace(6,11, "Java");
+
+        System.out.println(hello + ' ' + world);
+        System.out.println(one.compareTo(two));
+        System.out.println(builder);
+
     }
 
 }
