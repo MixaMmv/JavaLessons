31aa4b50a53eb9ce5b00b052eafbe792caba740c
MixaMmv <mmv.0097@gmail.com>
2021-09-29 11:17:49 +0500

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,45 @@
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class Main {
 
     public static void main(String[] args) {
+//        testArrList();
+        testLinkList();
+    }
 
-        String hello = "Hello";
-        String world = "world";
+    public static void testLinkList(){
+        LinkedList<Integer> linkedList = new LinkedList<>();
 
-        int a = 10;
+    }
 
-        double d1 = 1.00001;
-        double d2 = 1.00000999999;
+    public static void testArrList(){
+        ArrayList<Integer> arrayList = new ArrayList<>();
 
-        BigDecimal one = new BigDecimal(d1);
-        BigDecimal two = new BigDecimal(d2);
+        Integer a = 2;
+        arrayList.add(a);
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(1,3);
+        arrayList.add(4);
+        arrayList.add(5);
 
-        StringBuilder builder = new StringBuilder(hello + ' ' + world);
-        builder.replace(6,11, "Java");
+        for (Integer i : arrayList){
+            System.out.println(i);
+        }
 
-        System.out.println(hello + ' ' + world);
-        System.out.println(one.compareTo(two));
-        System.out.println(builder);
+        System.out.println(arrayList.get(0));
+        System.out.println(arrayList.indexOf(a));
+        System.out.println(arrayList.indexOf(999));
 
+        arrayList.remove(3);
+        arrayList.remove(a);
+
+        for (Integer i : arrayList){
+            System.out.println(i);
+        }
     }
 
 }
